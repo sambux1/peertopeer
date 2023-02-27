9bf5aafdfd0b32e551dcb382e1c32b5b5e75d7c3
sambux1 <sambuxbaum01@gmail.com>
2023-02-26 23:31:52 -0500

    Finished v0.1 unit tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,7 @@
 #include "unit_test_framework.h"
 #include "peer.h"
 
+#include<unistd.h>
 #include <iostream>
 #include <string>
 
@@ -21,6 +22,7 @@ TEST(create_nodes) {
 }
 
 // helper function to create nodes and fully connect them
+// receives pointers to the lists to fill, then fills the lists
 bool generate_connections_helper(int n,
                                  std::vector<Peer*>* peer_list,
                                  std::vector<contact_info*>* contact_infos) 
@@ -54,7 +56,7 @@ bool generate_connections_helper(int n,
 TEST(create_connections) {
     // this is the max for now
     // if we go any higher, the OS will stop allocating files
-    int n = 30;
+    int n = 20;
 
     std::vector<Peer*> peer_list;
     std::vector<contact_info*> contact_infos;
@@ -63,12 +65,49 @@ TEST(create_connections) {
     int ret = generate_connections_helper(n, &peer_list, &contact_infos);
     ASSERT_TRUE(ret);   // make sure it succeeded
 
-    std::cout << contact_infos.size() << std::endl;
-
     // check that each node is connected to n-1 other nodes
     for (int i = 0; i < n; i++) {
         int num_connections = peer_list[i]->get_num_connections();
         ASSERT_TRUE(num_connections == n-1);
+    }
+
+    // just make sure it got this far
+    ASSERT_TRUE(true);
+}
+
+// test that each node in an n-node network can broadcast and receive broadcasts
+// this also tests that nodes can send and receive
+TEST(broadcast) {
+    int n = 10;
+
+    std::vector<Peer*> peer_list;
+    std::vector<contact_info*> contact_infos;
+    
+    // create nodes and connect them
+    int ret = generate_connections_helper(n, &peer_list, &contact_infos);
+    ASSERT_TRUE(ret);   // make sure it succeeded
+
+    // have each node send a broadcast to each of its peers
+    for (int i = 0; i < n; i++) {
+        peer_list[i]->broadcast(std::to_string(i));
+        usleep(100000);
+    }
+
+    // check that each node received all broadcasts
+    for (int i = 0; i < n; i++) {
+        // check that there are n-1 messages received
+        int num_messages = peer_list[i]->get_num_messages();
+        ASSERT_TRUE(num_messages == n-1);
+
+        // check the content of the messages
+        for (int j = 0; j < n; j++) {
+            if (i == j)
+                continue;
+            message m = peer_list[i]->get_message();
+            // check that the message body is in ascending order
+            // this is slightly risky but it's fine
+            ASSERT_TRUE(std::stoi(m.body) == j);
+        }
     }
 
     // just make sure it got this far
